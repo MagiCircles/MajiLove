d10b8e5d36060c38ba4a07dd0764c2f83c8fdf0c
db0company <db0company@gmail.com>
2018-04-29 12:17:56 +0800

    Enable collections that are allowed as part of the Circles family

--- majilove/magicollections.py
+++ majilove/magicollections.py
@@ -1,7 +1,28 @@
-from magi.magicollections import MagiCollection, ActivityCollection as _ActivityCollection
+from magi.magicollections import MagiCollection, ActivityCollection as _ActivityCollection, BadgeCollection as _BadgeCollection, StaffConfigurationCollection as _StaffConfigurationCollection, DonateCollection as _DonateCollection
 
 ############################################################
 # Activities
 
 class ActivityCollection(_ActivityCollection):
     enabled = False
+
+############################################################
+# Badge Collection
+
+class BadgeCollection(_BadgeCollection):
+    enabled = True
+
+############################################################
+# Staff Configuration Collection
+
+class StaffConfigurationCollection(_StaffConfigurationCollection):
+    enabled = True
+
+############################################################
+# Donate Collection
+
+class DonateCollection(_DonateCollection):
+    enabled = True
+
+############################################################
+############################################################
