a82454f4b0b086e4e4e519b2738744cecde81fae
erys <12535744+erys@users.noreply.github.com>
2018-05-20 00:23:25 -0400

    Adding IdolCollection to magicollections.py

--- majilove/magicollections.py
+++ majilove/magicollections.py
@@ -1,3 +1,4 @@
+from django.utils.translation import ugettext_lazy as _
 from magi.magicollections import MagiCollection, ActivityCollection as _ActivityCollection, BadgeCollection as _BadgeCollection, StaffConfigurationCollection as _StaffConfigurationCollection, DonateCollection as _DonateCollection
 
 ############################################################
@@ -25,4 +26,19 @@ class DonateCollection(_DonateCollection):
     enabled = True
 
 ############################################################
+# Idol Collection
+
+class IdolCollection(MagiCollection):
+    queryset = models.Idol.objects.all()
+    title = _('Idol')
+    plural_title = _('Idols')
+    navbar_link_title = _('Characters')
+    # want the following as 'microphone' https://www.flaticon.com/free-icon/karaoke-microphone-icon_69364
+    icon = 'microphone'
+    navbar_link_list = 'utapri'
+    translated_fields = ('name', 'bio', 'star_sign', 'instrument', 'hometown')
+
+    reportable = False
+    blockable = False
+
 ############################################################
