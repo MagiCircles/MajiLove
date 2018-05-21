7f218d89d1bbccc5a8308752ce15ce6b6ea0c69d
erys <12535744+erys@users.noreply.github.com>
2018-05-20 23:58:54 -0400

    adding idol model to models.py (#1)
    * adding idol model to models.py
    
    * Making changes to idol model requested by db0
    
    * fixing typo: display_bloody_type -> display_blood_type
    
    * Adding Hobby field to Idol model

--- majilove/models.py
+++ majilove/models.py
@@ -1,11 +1,108 @@
-from django.contrib.auth.models import User
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext_lazy as _, string_concat, get_language
 from django.db import models
-from magi.item_model import MagiModel
+from django.conf import settings as django_settings
+from magi.models import User, uploadItem
+from magi.item_model import MagiModel, i_choices
 from magi.abstract_models import BaseAccount
 
-# Create your models here.
+
+############################################################
+# Utility stuff
+
+LANGUAGES_NEED_OWN_NAME = [ l for l in django_settings.LANGUAGES if l[0] in ['ru', 'zh-hans', 'zh-hant', 'kr'] ]
+ALL_ALT_LANGUAGES = [ l for l in django_settings.LANGUAGES if l[0] != 'en' ]
+
+
 
 class Account(BaseAccount):
     class Meta:
         pass
+
+############################################################
+# Idols
+
+class Idol(MagiModel):
+    collection_name = 'idol'
+
+    owner = models.ForeignKey(User, related_name='added_idols')
+
+    name = models.CharField(string_concat(_('Name'), ' (', _('Romaji'), ')'), max_length=100, unique=True)
+
+    japanese_name = models.CharField(string_concat(_('Name'), ' (', _('Japanese'), ')'), max_length=100, unique=True)
+
+    NAMES_CHOICES = LANGUAGES_NEED_OWN_NAME
+    d_names = models.TextField(_('Name'), null=True)
+
+    @property
+    def t_name(self):
+        if get_language() == 'ja':
+            return self.japanese_name
+        return self.names.get(get_language(), self.name)
+
+    romaji_voice_actor_name = models.CharField(_('Voice actor'), help_text='In romaji', max_length=100, null=True)
+
+    voice_actor_name = models.CharField(string_concat(_('Voice actor'), ' (', _('Japanese'), ')'), help_text='In Japanese characters.', max_length=100, null=True)
+
+    description = models.TextField(_('Description'), max_length=1000, null=True)
+    DESCRIPTIONS_CHOICES = ALL_ALT_LANGUAGES
+    d_descriptions = models.TextField(_('Description'), null=True)
+
+    # in cm
+    height = models.PositiveIntegerField(_('Height'), null=True)
+
+    weight = models.PositiveIntegerField(_('Weight'), null=True)
+
+    @property
+    def display_weight(self):
+        return self.weight or '?'
+
+    BLOOD_TYPE_CHOICES = (
+        'O',
+        'A',
+        'B',
+        'AB',
+    )
+
+    i_blood_type = models.PositiveIntegerField(_('Blood Type'), choices=i_choices(BLOOD_TYPE_CHOICES), null=True)
+
+    @property
+    def display_blood_type(self):
+        return self.blood_type or '?'
+
+    birthday = models.DateField(_('Birthday'), null=True, help_text='The year is not used, so write whatever')
+
+    ASTROLOGICAL_SIGN_CHOICES = (
+        ('Leo', _('Leo')),
+        ('Aries', _('Aries')),
+        ('Libra', _('Libra')),
+        ('Virgo', _('Virgo')),
+        ('Scorpio', _('Scorpio')),
+        ('Capricorn', _('Capricorn')),
+        ('Pisces', _('Pisces')),
+        ('Gemini', _('Gemini')),
+        ('Cancer', _('Cancer')),
+        ('Sagittarius', _('Sagittarius')),
+        ('Aquarius', _('Aquarius')),
+        ('Taurus', _('Taurus')),
+    )
+
+    i_astrological_sign = models.PositiveIntegerField(_('Astrological Sign'), choices=i_choices(ASTROLOGICAL_SIGN_CHOICES), null=True)
+
+    instrument = models.CharField(_('Instrument'), max_length=100, null=True)
+    INSTRUMENTS_CHOICES = ALL_ALT_LANGUAGES
+    d_instruments = models.TextField(_('Instrument'), null=True)
+
+    hometown = models.CharField(_('Hometown'), max_length=100, null=True)
+    HOMETOWNS_CHOICES = LANGUAGES_NEED_OWN_NAME
+    d_hometowns = models.TextField(_('Hometown'), null=True)
+
+    hobby = models.CharField(_('Hobby'), max_length=100, null=True)
+    HOBBYS_CHOICES = ALL_ALT_LANGUAGES
+    d_hobbys = models.TextField(_('Hobby'), null=True)
+
+    image = models.ImageField(_('Image'), upload_to=uploadItem('idol'))
+
+    small_image = models.ImageField(_('Image'), upload_to=uploadItem('idol/small'))
+
+    def __unicode__(self):
+        return unicode(self.t_name)
