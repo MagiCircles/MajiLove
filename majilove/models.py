9e8df8fe034d988e6e014dac6784e4bb8a0ac897
erys <12535744+erys@users.noreply.github.com>
2018-05-21 22:02:45 -0400

    Adding song model

--- majilove/models.py
+++ majilove/models.py
@@ -1,4 +1,6 @@
+from collections import OrderedDict
 from django.utils.translation import ugettext_lazy as _, string_concat, get_language
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.conf import settings as django_settings
 from magi.models import User, uploadItem
@@ -103,6 +105,78 @@ class Idol(MagiModel):
     image = models.ImageField(_('Image'), upload_to=uploadItem('idol'))
 
     small_image = models.ImageField(_('Image'), upload_to=uploadItem('idol/small'))
+
+    def __unicode__(self):
+        return unicode(self.t_name)
+
+
+############################################################
+# Songs
+
+class Song(MagiModel):
+    collection_name = 'song'
+    owner = models.ForeignKey(User, related_name='added_songs')
+
+    DIFFICULTY_VALIDATORS = [
+        MinValueValidator(1),
+        MaxValueValidator(13),
+    ]
+
+    name = models.CharField(string_concat(_('Title'), ' (', _('Translation'), ')'), max_length=100, null=True)
+    japanese_name = models.CharField(_('Title'), max_length=100, null=True)
+    NAMES_CHOICES = ALL_ALT_LANGUAGES
+    d_names = models.TextField(_('Title'), null=True)
+    @property
+    def t_name(self):
+        if get_language() == 'ja': return self.japanese_name
+        return self.names.get(get_language(), self.name)
+
+    image = models.ImageField('Album cover', upload_to=uploadItem('song'), null=True)
+
+    composer = models.CharField(_('Composer'), max_length=100, null=True)
+    COMPOSERS_CHOICES = LANGUAGES_NEED_OWN_NAME
+    d_composers = models.TextField(_('Composer'), null=True)
+    lyricist = models.CharField(_('Lyricist'), max_length=100, null=True)
+    LYRICISTS_CHOICES = LANGUAGES_NEED_OWN_NAME
+    d_lyricists = models.TextField(_('Lyricist'), null=True)
+    arranger = models.CharField(_('Arranger'), max_length=100, null=True)
+    ARRANGERS_CHOICES = LANGUAGES_NEED_OWN_NAME
+    d_arrangers = models.TextField(_('Arranger'), null=True)
+
+    singers = models.ManyToManyField(Idol, related_name="sung_songs", verbose_name=_('Singers'))
+
+    # TODO: whichever of photo or song goes in second should take this from the other
+    COLORS= OrderedDict([
+        (1, { # yellow
+            'translation': _('Star'),
+            'english': u'Star'
+        }),
+        (2, { # red
+            'translation': _('Shine'),
+            'english': u'Shine'
+        }),
+        (3, { # blue
+            'translation': _('Dream'),
+            'english': u'Dream'
+        })
+    ])
+
+    COLOR_CHOICES = [(_name, _info['translation']) for _name, _info in COLORS.items()]
+    COLOR_WITHOUT_I_CHOICES=True
+    i_color = models.PositiveIntegerField(_('Color'), choices=i_choices(COLOR_CHOICES))
+
+    easy_notes = models.PositiveIntegerField(string_concat(_('Easy'), ' - ', _('Notes')), null=True)
+    easy_difficulty = models.PositiveIntegerField(string_concat(_('Easy'), ' - ', _('Difficulty')), validators=DIFFICULTY_VALIDATORS, null=True)
+    normal_notes = models.PositiveIntegerField(string_concat(_('Normal'), ' - ', _('Notes')), null=True)
+    normal_difficulty = models.PositiveIntegerField(string_concat(_('Normal'), ' - ', _('Difficulty')), validators=DIFFICULTY_VALIDATORS, null=True)
+    hard_notes = models.PositiveIntegerField(string_concat(_('Hard'), ' - ', _('Notes')), null=True)
+    hard_difficulty = models.PositiveIntegerField(string_concat(_('Hard'), ' - ', _('Difficulty')), validators=DIFFICULTY_VALIDATORS, null=True)
+    pro_notes = models.PositiveIntegerField(string_concat(_('Pro'), ' - ', _('Notes')), null=True)
+    pro_difficulty = models.PositiveIntegerField(string_concat(_('Pro'), ' - ', _('Difficulty')), validators=DIFFICULTY_VALIDATORS, null=True)
+
+    length = models.PositiveIntegerField(_('Length'), null=True)
+
+    #TODO: unlock method
 
     def __unicode__(self):
         return unicode(self.t_name)
